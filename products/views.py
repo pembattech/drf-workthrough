70d9522003ac8abd20b9d2af235da42e70ba5700
pembattech <pembat.tech@gmail.com>
2024-02-05 14:09:55 +0545

    Learn generics and update urls, settings and py_clients for integration.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from rest_framework import generics
+
+from .models import Product
+from .serializers import ProductSerializer
 
 # Create your views here.
+
+class ProductCreateAPIView(generics.CreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+
+        print(serializer.validated_data)
+
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        
+        if content is None:
+            content = title
+            
+            
+        serializer.save(content = content)
+class ProductDetailAPIView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+class ProductDetailAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+    
